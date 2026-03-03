49b1eecc3555e628967ec1cd4885434ac6b00eee
isti481 <juhasz.istvan4@hotmail.com>
2026-03-03 21:38:54 +0100

    fix javascript undefined
    problem because of login system, if not logged console emessage so i put the event after the "draw"

--- Microtravel/Microtravel/wwwroot/js/site.js
+++ Microtravel/Microtravel/wwwroot/js/site.js
@@ -2,37 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-
-let timer;
-
-document.getElementById("searchBox").addEventListener("keyup", function () {
-
-
-clearTimeout(timer);
-
-let value = this.value.trim();
-    console.log(value);
-    if (value.length == 0) {
-        render(value);
-        return;
-    }
-    if (value.length < 3) { return }
-    
-
-    render(value);
-
-});
-
-
-function render(value) {
-    timer = setTimeout(() => {
-
-        fetch(`/Travels/Search?searchString=${value}`)
-            .then(res => res.text())
-            .then(html => {
-                document.getElementById("travelContainer").innerHTML = html;
-            });
-
-    }, 400);
-}
